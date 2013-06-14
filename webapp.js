5d38bc397410c1c634d92110788251990003b3e4
Peter Braden <peterbraden@peterbraden.co.uk>
2013-06-14 14:33:37 -0700

    WIP on strider plugin

--- webapp.js
+++ webapp.js
@@ -1,6 +1,124 @@
 var path = require('path')
 
 module.exports = function(ctx, cb){
+  /*
+   * GET /api/jelly/
+   *
+   * Get the current Strider config for specified project. This will be a JSON-encoded
+   * object with the keys: 'test_files'
+   *
+   * @param url Github html_url of the project.
+   */
+  function getIndex(req, res) {
+    var url = req.param("url")
+
+    function error(err_msg) {
+      console.error("Strider-Jelly: getIndex() - %s", err_msg)
+      var r = {
+        errors: [err_msg],
+        status: "error"
+      }
+      res.statusCode = 400
+      return res.end(JSON.stringify(r, null, '\t'))
+    }
+
+    req.user.get_repo_config(url, function(err, repo, access_level, owner_user_obj) {
+      if (err) {
+        return error("Error fetching Repo Config for url " + url + ": " + err)
+      }
+      var r = {
+        status: "ok",
+        errors: [],
+        results: {
+          url: repo.get("jelly_url")
+        , port: repo.get("jelly_port")
+        , payload: repo.get("jelly_payload")
+        , serve_static: repo.get("jelly_static")
+        , static_dir: repoget("jelly_static_dir")
+        }
+      }
+      return res.end(JSON.stringify(r, null, '\t'))
+    })
+  }
+
+  /*
+   * POST /api/jelly/
+   *
+   * Set the current Strider config for specified project.
+   *
+   * @param url Github html_url of the project.
+   *
+   */
+  function postIndex(req, res) {
+    var url = req.param("url")
+      , jelly_url = req.body["jelly_url"]
+      , jelly_port = req.body["jelly_port"]
+
+    function error(err_msg) {
+      console.error("Strider-QUnit: postIndex() - %s", err_msg)
+      var r = {
+        errors: [err_msg],
+        status: "error"
+      }
+      res.statusCode = 400
+      return res.end(JSON.stringify(r, null, '\t'))
+    }
+
+    req.user.get_repo_config(url, function(err, repo, access_level, owner_user_obj) {
+      if (err) {
+        return error("Error fetching Repo Config for url " + url + ": " + err)
+      }
+      // must have access_level > 0 to be able to continue;
+      if (access_level < 1) {
+        console.debug(
+          "User %s tried to change qunit config but doesn't have admin privileges on %s (access level: %s)",
+          req.user.email, url, access_level);
+        return error("You must have access level greater than 0 in order to be able to configure qunit.");
+      }
+      var q = {$set:{}}
+      if (path) {
+        repo.set('qunit_path', path)
+      }
+      if (file) {
+        repo.set('qunit_file', file)
+      }
+
+      var r = {
+        status: "ok",
+        errors: [],
+        results: {
+          files: repo.get('qunit_file'),
+          path: repo.get('qunit_path')
+        }
+      }
+      if (file || path) {
+        req.user.save(function(err) {
+            if (err) {
+              var errmsg = "Error saving qunit config " + req.user.email + ": " + err;
+              return error(errmsg)
+            }
+            return res.end(JSON.stringify(r, null, '\t'))
+        })
+      } else {
+        return res.end(JSON.stringify(r, null, '\t'))
+      }
+    })
+
+  }
+
+
+  // Extend RepoConfig model with 'Jelly' properties
+  function jellyPlugin(schema, opts) {
+    schema.add({
+      jelly_url: String,
+      jelly_port: Number,
+      jelly_payload: String,
+      jelly_static: Boolean
+      jelly_static_dir: String
+    })
+  }
+  ctx.models.RepoConfig.plugin(jellyPlugin)
+
   // Add panel HTML snippet for project config page
   ctx.registerPanel('project_config', {
     src: path.join(__dirname, "templates", "project_config.html"),
---
+++ worker.js
@@ -0,0 +1,75 @@
+var jp = require('jelly-proxy')
+
+module.exports = function(ctx, cb){
+  var proxyServer
+    , staticServer
+
+  var prepare = function(ctx, cb){
+
+    var handleProgress = function(req, res){
+      console.log("[JELLY:Progress]", req);// TODO Req params
+      ctx.striderMessage("[JELLY:Progress]", req)
+    }
+
+    var handleResults = function(req, res){
+      console.log("[JELLY:Results]", req);// TODO Req params
+      ctx.events.emit("testDone", res) 
+    }
+
+    var routes = {
+      "_jelly/progress" : handleProgress
+    , "_jelly/results" : handleResults
+    }
+
+    var handleReq = function(req, res){
+     if (routes[req.url]){
+       routes[req.url](req, res)
+     } else {
+      res.writeHead(404);
+      res.end()
+     }
+    }
+
+    var repoconf = ctx.jobData.repo_config
+      , payload = repoconf.jelly_payload
+      , port = repoconf.jelly_port
+      , jellyurl = repoconf.jelly_url
+      , uri = url.parse(jellyurl) 
+      , proxyport = uri.port || 80
+      , serveStatic = repoconf.jelly_static
+
+    ctx.browsertestPort = port
+    ctx.browsertestPath = jellyurl
+
+    var opts = {
+      payload : payload
+    }
+
+    proxyServer = jp(opts, handleReq, proxyport)
+    proxyServer.listen(port);
+    
+    if (serveStatic){
+      var send = require('send')
+        , http = require('http')
+
+      staticServer = http.createServer(function(rq, rs){
+        send(rq, rq.url).pipe(rs); 
+      })
+      staticServer.listen(proxyport)
+    }
+
+  }
+
+  var cleanup = function(ctx, cb){
+    proxyServer.close()
+    if (staticServer)
+      staticServer.close()
+  }
+
+  ctx.addBuildHook({
+    prepare: prepare
+  , cleanup: cleanup 
+  })
+  console.log("Jelly Proxy loaded")
+  cb(null);
+}
